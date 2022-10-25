bd2abb1dbeca0dc0dddf0dbb254b0d6ac8f73fcf
Luigi Sportelli <l.sportelli@entando.com>
2022-10-25 18:28:22 +0200

    first commit

---
+++ api/v1alpha1/entandoupgrade_types.go
@@ -0,0 +1,66 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// EntandoUpgradeSpec defines the desired state of EntandoUpgrade
+type EntandoUpgradeSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// FromVersion is an example field of EntandoUpgrade. Edit entandoupgrade_types.go to remove/update
+	FromVersion string `json:"fromVersion"`
+	// ToVersion is an example field of EntandoUpgrade. Edit entandoupgrade_types.go to remove/update
+	ToVersion string `json:"toVersion"`
+}
+
+// EntandoUpgradeStatus defines the observed state of EntandoUpgrade
+type EntandoUpgradeStatus struct {
+	Progress string `json:"progress"`
+	Patch    string `json:"patch"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// EntandoUpgrade is the Schema for the entandoupgrades API
+type EntandoUpgrade struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   EntandoUpgradeSpec   `json:"spec,omitempty"`
+	Status EntandoUpgradeStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// EntandoUpgradeList contains a list of EntandoUpgrade
+type EntandoUpgradeList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []EntandoUpgrade `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&EntandoUpgrade{}, &EntandoUpgradeList{})
+}
