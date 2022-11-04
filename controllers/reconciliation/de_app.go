30a3058f6b9d5cfbdae8d847ce0b472acf3df0bf
firegloves <lucorset@gmail.com>
2022-11-04 17:02:33 +0100

    de app reconciliation

--- controllers/reconciliation/de_app.go
+++ controllers/reconciliation/de_app.go
@@ -3,14 +3,26 @@ package reconciliation
 import (
 	"context"
 	"github.com/entgigi/upgrade-operator.git/api/v1alpha1"
-	"time"
-
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 func (r *ReconcileManager) reconcileDeApp(ctx context.Context, image string, req ctrl.Request, cr *v1alpha1.EntandoAppV2) error {
 	r.Log.Info("Starting DeApp reconciliation flow")
-	time.Sleep(time.Second * 5)
+
+	deployment, err := r.mustGetDeployment(ctx, cr.Spec.EntandoAppName, "")
+	if err != nil {
+		return err
+	}
+
+	deployment = r.updateCommonDeploymentData(deployment,
+		image,
+		cr.Spec.CommonEnvironmentVariables,
+		cr.Spec.DeApp.EnvironmentVariables)
+
+	if err = r.Update(ctx, deployment); err != nil {
+		return err
+	}
+
 	r.Log.Info("Finished DeApp reconciliation flow")
 
 	return nil
